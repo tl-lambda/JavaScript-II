768bd7bc534fc9e4ec8a6de2762b31aa76569b39
lanners-marshall <lanners.marshall@gmail.com>
2019-07-30 10:41:59 -0700

    update

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,23 +27,29 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.lenght - 1)
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let bool = list.includes(item)
+  cb(bool)
 }
 
 /* STRETCH PROBLEM */
@@ -52,4 +58,19 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+
+  const lookup = {}, ar2 = []
+
+  lookup.forEach(item => {
+    !lookup[item] && (ar2.push(item))
+    lookup[item] = true
+  })
+
+  cb(ar2)
 }
+
+
+
+
+
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,17 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const closure = () => {
+	let name = 'dude'
+
+	function greetDude(){
+		console.log("what up " + name)
+	}
+	return greetDude();
+}
+
+closure()
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,14 +19,47 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0
+  return () => {
+    counter = counter + 1
+    console.log(counter)
+    return counter
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+let aCounter = counter()
+aCounter()
+aCounter()
+aCounter()
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+let x = 0;
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let newObj = {};
+  newObj.increment = function(){
+  	x++;
+  	return x;
+  }
+
+  newObj.decrement = function(){
+  	x--;
+  	return x;
+  }
+
+  return newObj
 };
+
+let newCounter = counterFactory();
+console.log(newCounter.decrement());
+console.log(newCounter.decrement())
+console.log(newCounter.increment())
+
+
+
+
+
+
+
